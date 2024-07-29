b6abef1a982c7c34a52db650ca2899d01131cbb2
fedevilloria <fedevilloria@hotmail.com>
2024-07-29 08:35:50 -0300

    .

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from gestionPedidos.models import Clientes
+from gestionPedidos.models import Articulos
+from gestionPedidos.models import Pedidos
+
 # Register your models here.
+
+# list_display -> Sirve para ver mas de un campo en el registro del panel de administracion, por parametro recibe las variables escritas en "models" que queremos ver.
+# search_fields -> Sirve para agregar una barra de busqueda en el panel de administracion, por parametro recibe el filtro que queremos buscar.
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "email"]
+    search_fields = ["nombre"]
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -24,8 +24,11 @@ models.EmailField() = tipo de dato en este caso es el de email el cual verificar
 class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     telefono = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.nombre
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
